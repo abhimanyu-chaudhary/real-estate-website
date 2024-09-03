70062a3d3d9534520df94d6f45ecb441217d14b5
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-09-03 18:33:27 +0545

    components created

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+// import Logo from './../../public/site-logo.svg'
 
 function Routing() {
   return (
-    <div>
-      
+    <div className='bg-slate-500'>
+      <div>
+        <div>
+            <Link to='/'>
+                <img src={Logo} alt="" />
+            </Link>
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+      <Routes>
+        <h1>Hello World!</h1>
+        <Route path='' element={<></>} />
+      </Routes>
     </div>
   )
 }
 
-export default Routing
+export default Routing;
