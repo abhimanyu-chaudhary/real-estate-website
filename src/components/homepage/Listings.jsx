d3c826a7501298d5ed78e6c9b9a2cb88583ae723
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-09-05 00:39:34 +0545

    Update Listings.jsx

--- src/components/homepage/Listings.jsx
+++ src/components/homepage/Listings.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Listings() {
     const listingData = [
-        {name: "Grand Family House", location: }
+        {name: "Grand Family House", location: "Kathmandu"}
     ]
 
   return (
