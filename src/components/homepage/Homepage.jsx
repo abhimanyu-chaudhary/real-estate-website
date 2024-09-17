1d400cdf521f4524db0fe6a272fde5d3ab618f29
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-09-17 18:54:37 +0545

    new section added

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Discover from './Discover';
 import Listings from './Listings';
 import Accordion from './Accordion';
+import Loan from './Loan';
 
 function Homepage() {
   return (
     <div className='w-[100vw]'>
       <Discover />
       <Listings />
-      
+      <Loan />
     </div>
   )
 }
---
+++ src/components/homepage/Loan.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Loan() {
+  return (
+    <div className='w-[1245px] absolute top-[2432px] left-[104px] h-[200px] flex justify-between items-center'>
+      <div>
+        <h1 className='w-[500px] text-[40px] font-semibold leading-tight'>Need A Home Loan? Get Pre-Approved</h1>
+        <p className='w-[500px]'>Creating a very beautiful website design in accordance with the fundamental user experience which is examined more deeply by the UX Designers that we have. 
+        And make good visuals so that clients are satisfied and easy when viewing the website. First impressions are our tricks to attract a customer who has seen the website that we are going to create
+        </p>
+      </div>
+      <div className='w-[608px] h-[692px] rounded-tl-[70px] rounded-bl-[70px] bg-[#27AD77]'>
+        <div className='w-[600px] h-[500px] bg-[#E6E6E6] relative right-[100px] top-[100px] rounded-[16px]'>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Loan;
