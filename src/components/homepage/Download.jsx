9c9e22ec077e451c9460a0b0512675a250e536de
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-09-26 20:52:24 +0545

    download section

--- src/components/homepage/Download.jsx
+++ src/components/homepage/Download.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import iphone from './../../../public/iphone.png'
+import screen from './../../../public/screen.png'
+import appStore from './../../../public/Appstore Logo.png'
+import playStore from './../../../public/Google Play Logo.png' 
 
 function Download() {
   return (
-    <div className='w-[1245px] h-[792px] absolute top-[6200px]  flex justify-between items-center mb-40'>
+    <div className='w-[1245px] h-[792px] absolute top-[5900px] flex gap-[200px] items-center mb-40'>
       <div className='w-[408px] h-[792px] rounded-tr-[70px] rounded-br-[70px] bg-[#27AD77]'>
         <div className='relative top-[40px] left-[150px] rounded-[16px]'>
-            <img className='object-fill rounded-[16px] w-[357px] h-[712px] ' src={iphone} alt="" />
+            <img className='object-fill  w-[357px] h-[712px] shadow-custom-shadow rounded-[65px]' src={iphone} alt="" />
+            <img className='absolute top-[24px] left-[25px]' src={screen} alt="" />
         </div>
       </div>
       <div>
-        <h1 className='w-[500px] text-[40px] font-semibold leading-tight '>Need A Home Loan? Get Pre-Approved</h1>
-        <p className='w-[500px] py-5'>Need a home loan? Get pre-approved today! Our streamlined process makes it easy to secure financing before you start searching for your dream home. With competitive rates, flexible terms, and expert support, we guide you every step of the way.</p>
+        <h1 className='w-[500px] text-[40px] font-semibold leading-tight '>Download The Application TO Find Your Dream House</h1>
+        <p className='w-[500px] py-5'>Looking for your perfect home? Our app makes it easier than ever! Explore a wide range of properties tailored to your needs with just a few taps. Whether you're searching for a cozy apartment, a spacious family home, or a luxury estate, our app provides detailed listings, virtual tours, and personalized recommendations to help you find your dream house.</p>
         <p className='w-[500px]'>
-        Pre-approval gives you the confidence to make an offer quickly, knowing exactly what you can afford. Don’t wait-start your pre-approval now for a hassle-free home-buying experience!
+        Download now and start your journey to your ideal home today!
         </p>
-        <button className='w-[151px] h-[56px] bg-[#FF8A20] rounded-[8px] mt-5 text-white font-semibold'>Read More</button>
+        <div className='flex my-8'>
+            <img src={playStore} alt="" />
+            <img src={appStore} alt="" />
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,8 @@ export default {
       },
       boxShadow: {
         'custom-shadow': '0px 0px 9px 1px rgba(0, 0, 0, 0.75)',
+        'custom-shadow-1': '25.71px 46.27px 61.7px rgba(157, 149, 140, 0.2)',  // First shadow
+        'custom-shadow-2': '15.42px 15.42px 25.71px rgba(154, 151, 142, 0.15)' // Second shadow
       }
     }
   },
