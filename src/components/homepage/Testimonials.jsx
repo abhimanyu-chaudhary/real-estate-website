e44bf5c604211f7d2a65e9d83c7aa66abeef2598
Abhimanyu Chaudhary <146760746+abhimanyu-chaudhary@users.noreply.github.com>
2024-09-26 19:50:27 +0545

    Update Testimonials.jsx

--- src/components/homepage/Testimonials.jsx
+++ src/components/homepage/Testimonials.jsx
@@ -12,7 +12,7 @@ function Testimonials() {
       matter:
         "Staying here was an absolute delight! The house is spacious, cozy, and perfect for our family. We felt right at home from the moment we walked in. Highly recommend for anyone looking for comfort and convenience!",
       image:
-        "https://images.unsplash.com/photo-1600209142000-aa256622e64a?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1602872029708-84d970d3382b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       id: 2,
@@ -57,7 +57,7 @@ function Testimonials() {
       heading: "Charming House in a Prime Location",
       matter:
         "We were so impressed with the location and the house itself. Everything is nearby—shops, parks, schools—yet the neighborhood is quiet and peaceful. The house is clean, well-maintained, and perfect for a growing family.",
-      
+      image: "https://images.unsplash.com/photo-1615874694520-474822394e73?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
       id: 7,
@@ -66,6 +66,7 @@ function Testimonials() {
       heading: "Spacious and Inviting Home",
       matter:
         "This house felt like home from the minute we walked in. It’s spacious, with a lot of natural light, and has been thoughtfully designed for families. We love how everything is so accessible, and the quality of the house is excellent.",
+      image: "https://images.unsplash.com/photo-1616594039964-ae9021a400a0?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
       id: 8,
@@ -74,6 +75,7 @@ function Testimonials() {
       heading: "Elegant and Family-Friendly House",
       matter:
         "We are so happy with our decision to move here! The house is elegant, well-located, and perfect for family life. The open floor plan and spacious rooms made us feel at ease right away. Highly recommend to anyone looking for a family home!",
+      image: "https://images.unsplash.com/photo-1616486701797-0f33f61038ec?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
       id: 9,
@@ -82,6 +84,7 @@ function Testimonials() {
       heading: "A Dream Home for the Family",
       matter:
         "We’ve found our dream home! It’s a perfect balance of modern design and cozy comfort. The neighborhood is fantastic, and the house itself offers everything a family needs. We couldn’t be happier with our decision!",
+      image: "https://images.unsplash.com/photo-1615875474908-f403116f5287?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
   ];
 
@@ -94,7 +97,7 @@ function Testimonials() {
 
   return (
     <div >
-        <div className="w-[1349px] h-[806px] absolute top-[4900px] bg-[#27AD77] flex justify-center items-center px-20">
+        <div className="w-[1349px] h-[806px] absolute top-[4900px] bg-[#27AD77] flex justify-center items-center px-20 select-none">
             <div className="w-[700px]">
               <h1 className="text-[40px] font-semibold w-[75%] leading-tight text-white min-h-[90px]">
                 {data[num].heading}
